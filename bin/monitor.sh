6371e83604d4983e55581cab8e7907f84d428f60
i13acx2o <i13acx2o.dev@gmail.com>
2023-12-20 04:14:07 +0200

    fix: correct monitor names

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -7,16 +7,16 @@ choice=$(echo $menu | rofi -dmenu -i -p "Monitor")
 # Execution
 case "$choice" in
     "Builtin")
-	xrandr --output eDP-1 --mode 1920x1080 --output DP-2 --off
+	xrandr --output eDP1 --mode 1920x1080 --output DP2 --off
 	;;
     "External")
-	xrandr --output DP-2 --mode 1920x1080 --output eDP-1 --off
+	xrandr --output DP2 --mode 1920x1080 --output eDP1 --off
 	;;
     "Mirror")
-	xrandr --output DP-2 --mode 1920x1080 --output eDP-1 --mode 1920x1080 --same-as DP-2
+	xrandr --output DP2 --mode 1920x1080 --output eDP1 --mode 1920x1080 --same-as DP2
 	;;
     "Extend")
-	xrandr --output DP-2 --mode 1920x1080 --output eDP-1 --mode 1920x1080 --right-of DP-2
+	xrandr --output DP2 --mode 1920x1080 --output eDP1 --mode 1920x1080 --right-of DP2
 	;;
     *)
         echo "Invalid option. Exiting."
